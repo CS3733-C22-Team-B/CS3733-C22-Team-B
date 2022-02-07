73543536bceec8d1495772eefd70632b8b2eabfa
dragonf17 <30739405+dragonf17@users.noreply.github.com>
2022-02-06 23:27:29 -0500

    points loaded

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/MapEditorController.java
@@ -1,31 +1,21 @@
 package edu.wpi.cs3733.c22.teamB.controllers;
 
 import edu.wpi.cs3733.c22.teamB.Bapp;
+import edu.wpi.cs3733.c22.teamB.entity.Location;
+import edu.wpi.cs3733.c22.teamB.entity.LocationDBI;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+import java.util.List;
+
 public class MapEditorController {
-
-    @FXML
-    Circle testPoint0 = new Circle(0,0,15);
-
-    @FXML
-    Circle testPoint1 = new Circle(1920,1080,15);
-
-    @FXML
-    Circle testPoint2 = new Circle(0,1080,15);
-
-    @FXML
-    Circle testPoint3 = new Circle(1920,0,15);
-
 
 
     @FXML
     private AnchorPane anchorPane;
-
 
     @FXML
     private ImageView imageView;
@@ -34,7 +24,6 @@ public class MapEditorController {
         final int imageHeight = 5000;
         double x = imageView.getFitWidth();
         double addX = imageView.getLayoutX();
-        System.out.println("x + " + addX);
         return ((x/imageHeight)*desiredX) + addX;
     }
 
@@ -42,28 +31,38 @@ public class MapEditorController {
         final int imageHeight = 5000;
         double x = imageView.getFitWidth();
         double addY = imageView.getLayoutY();
-        System.out.println("y + " + addY);
         return ((x/imageHeight)*desiredY) + addY;
 
     }
 
 
+    public void addPoint(double x, double y, Color color){
+        Circle testPoint = new Circle(getImageX(x),getImageY(y),3);
+        anchorPane.getChildren().add(testPoint);
+        testPoint.setFill(color);
+    }
 
+    public void addPoints(){
+        LocationDBI locationDBI = new LocationDBI();
+        List<Location> locationList = locationDBI.getAllNodes();
+        for(Location local: locationList){
+            double x = local.getXcoord();
+            double y = local.getYcoord();
+            addPoint(x,y,Color.BLACK);
+        }
+    }
 
 
 
 
     @FXML
     public void initialize(){
-        //imageView.setFitHeight(Bapp.getPrimaryStage().getOutputScaleY()-50);
         //Bapp.getPrimaryStage().setFullScreen(false);
         System.out.println(imageView.fitHeightProperty().get());
-        Circle testPoint4 = new Circle(getImageX(0),getImageY(0),3);
-        anchorPane.getChildren().add(testPoint4);
-        Circle testPoint5 = new Circle(getImageX(5000),getImageY(3400),3);
-        anchorPane.getChildren().add(testPoint5);
-        testPoint4.setFill(Color.PURPLE);
-        testPoint5.setFill(Color.ORANGE);
+        addPoint(0,0,Color.ORANGE);
+        addPoint(5000,3400, Color.RED);
+        addPoints();
+
     }
 
 }
