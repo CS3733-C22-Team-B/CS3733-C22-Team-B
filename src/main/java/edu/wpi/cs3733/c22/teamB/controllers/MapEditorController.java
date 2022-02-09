0c8fc257ed907bebe995231568ae346c22244e08
dragonf17 <30739405+dragonf17@users.noreply.github.com>
2022-02-08 19:17:07 -0500

    second attempt

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/MapEditorController.java
@@ -23,6 +23,7 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -56,6 +57,14 @@ public class MapEditorController {
     MedicalEquipmentDBI medicalDBI = new MedicalEquipmentDBI();
     List<MedicalEquipment> medicalList = medicalDBI.getAllNodes();
     CSVRestoreBackupController backupper = new CSVRestoreBackupController();
+    LocationParserI locParser = new LocationParserI();
+    EmployeeParserI employeeParserI = new EmployeeParserI();
+    ExternalTransportSRParserI extTransSRParserI = new ExternalTransportSRParserI();
+    FoodDeliveryParserI foodDeliveryParserI = new FoodDeliveryParserI();
+    MedicalEquipmentSRParserI medicalEquipmentSRParserI = new MedicalEquipmentSRParserI();
+    MedicalEquipmentParserI medicalEquipmentParserI = new MedicalEquipmentParserI();
+    MedicineDeliverySRParserI medicineDeliverySRParserI = new MedicineDeliverySRParserI();
+    DatabaseManager databaseManager = new DatabaseManager();
     String currentFloor = "3";
     boolean addState = false;
 
@@ -77,8 +86,7 @@ public class MapEditorController {
     @FXML
     private JFXButton modifyButton;
 
-    @FXML
-    private JFXButton loadFromCSV;
+
 
     @FXML
     private JFXButton submitModifyButton;
@@ -238,28 +246,57 @@ public class MapEditorController {
         addPoints();
     }
 
+
     @FXML public void loadFromCSV(){
         try {
-            DatabaseManager databaseManager = new DatabaseManager();
-            databaseManager.createTable();
+            String pathString = new File("").getAbsolutePath();
+            File f = new File(pathString);
 
-            CSVReader2 reader = new CSVReader2();
+            File backDir = new File(f.getAbsolutePath() + "/backup");
 
-            List<String> locationList = reader.firstRestore("TowerLocationsB.csv");
-            List<String> employeeList = reader.firstRestore("EmployeeB.csv");
-            List<String> externalTransportList = reader.firstRestore("ExternalTransportSRB.csv");
-            List<String> foodDeliveryList = reader.firstRestore("FoodDeliverySRB.csv");
-            List<String> equipmentList = reader.firstRestore("MedicalEquipmentB.csv");
-            List<String> equipmentSRList = reader.firstRestore("MedicalEquipmentSRB.csv");
-            List<String> medicineDeliveryList = reader.firstRestore("MedicineDeliverySRB.csv");
+            File filePathLocation = new File(backDir.getAbsolutePath() + "/TowerLocationsB.csv");
 
-            LocationParserI locParser = new LocationParserI();
-            EmployeeParserI employeeParserI = new EmployeeParserI();
-            ExternalTransportSRParserI extTransSRParserI = new ExternalTransportSRParserI();
-            FoodDeliveryParserI foodDeliveryParserI = new FoodDeliveryParserI();
-            MedicalEquipmentSRParserI medicalEquipmentSRParserI = new MedicalEquipmentSRParserI();
-            MedicalEquipmentParserI medicalEquipmentParserI = new MedicalEquipmentParserI();
-            MedicineDeliverySRParserI medicineDeliverySRParserI = new MedicineDeliverySRParserI();
+            File filePathEmployee = new File(backDir.getAbsolutePath() + "/EmployeeB.csv");
+
+            File filePathExtTrans = new File(backDir.getAbsolutePath() + "/ExternalTransportSRB.csv");
+
+            File filePathFood = new File(backDir.getAbsolutePath() + "/FoodDeliverySRB.csv");
+
+            File filePathEq = new File(backDir.getAbsolutePath() + "/MedicalEquipmentB.csv");
+
+            File filePathEqSR = new File(backDir.getAbsolutePath() + "/MedicalEquipmentSRB.csv");
+
+            File filePathMedSR = new File(backDir.getAbsolutePath() + "/MedicineDeliverySRB.csv");
+
+            CSVReader2 reader = new CSVReader2(filePathLocation);
+
+            // Read Location CSV
+            reader.setFile(filePathLocation);
+            List<String> locationList = reader.read();
+
+            // Read Employee CSV
+            reader.setFile(filePathEmployee);
+            List<String> employeeList = reader.read();
+
+            // Read ExternalTrans CSV
+            reader.setFile(filePathExtTrans);
+            List<String> externalTransportList = reader.read();
+
+            // Read FoodDelivery CSV
+            reader.setFile(filePathFood);
+            List<String> foodDeliveryList = reader.read();
+
+            // Read Equipment CSV
+            reader.setFile(filePathEq);
+            List<String> equipmentList = reader.read();
+
+            // Read EquipmentSR CSV
+            reader.setFile(filePathEqSR);
+            List<String> equipmentSRList = reader.read();
+
+            // Read MedicineDelivery CSV
+            reader.setFile(filePathMedSR);
+            List<String> medicineDeliveryList = reader.read();
 
             List<Location> locationList1 = locParser.fromStringsToObjects(locationList);
             List<Employee> employeeList1 = employeeParserI.fromStringsToObjects(employeeList);
@@ -271,7 +308,6 @@ public class MapEditorController {
 
             databaseManager.restoreTables(locationList1, employeeList1, externalTransportSRList1, medicineDeliverySRList1,
                     foodDeliverySRList1, medicalEquipmentSRList1, medicalEquipmentList1);
-            refresh();
         } catch (IOException ex){
             ex.printStackTrace();
         }
