5191a4e43b34e50ae0687a6ba13aed8192ae5064
idaate <72719123+idaate@users.noreply.github.com>
2022-02-13 18:09:49 -0500

    worked on login screen

--- src/main/java/edu/wpi/cs3733/c22/teamB/entity/GiftFloralSR.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/entity/GiftFloralSR.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class GiftFloralSR extends AbstractSR {
 
+    // maybe?? add employee? but its probably too late
+
+    // things that need to be added in all of these;
+    // srID, status, location, requestor, assignedEmployee, dateRequested,
+    // notes, srType
+
     String gift;
     String deliveryDate;
     String deliveryRoom;
@@ -26,7 +32,33 @@ public class GiftFloralSR extends AbstractSR {
 
     public GiftFloralSR(String srID, String statusStr) {
         super(srID, statusStr);
+        this.deliveryRoom = null;
     }
+
+    // Getters and Setters
+
+    public String getSRID() { return this.srID; }
+
+    public String getGift() { return gift; }
+    public void setGift(String gift) { this.gift = gift; }
+
+    public String getDeliveryDate() { return deliveryDate; }
+    public void setDeliveryDate(String deliveryDate) { this.deliveryDate = deliveryDate; }
+
+    public String getDeliveryRoom() { return deliveryRoom; }
+    public void setDeliveryRoom(String deliveryRoom) { this.deliveryRoom = deliveryRoom; }
+
+    @Override
+    public String toString(){
+        return "GiftFloralSR{" +
+                "srID='" + srID + '\'' +
+                ", status=" + status + '\'' +
+                ", gift=" + gift + '\'' +
+                ", deliveryDate=" + deliveryDate + '\'' +
+                ", deliveryRoom=" + deliveryRoom + '\'' +
+                '}';
+    }
+
 }
 
 
