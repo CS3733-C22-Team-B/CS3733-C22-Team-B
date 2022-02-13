8513bc7fb017d1ff6fe947e0e7dc99249458e3b1
dtbpkmte <dtbpkmte@gmail.com>
2022-02-13 16:01:29 -0500

    integrated with home page

--- src/main/java/edu/wpi/cs3733/c22/teamB/Bapp.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/Bapp.java
@@ -60,8 +60,7 @@ public class Bapp extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Login.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/ServiceRequestMenu.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Login.fxml"));
         _primaryStage = primaryStage;
 
         Parent root = loader.load();
