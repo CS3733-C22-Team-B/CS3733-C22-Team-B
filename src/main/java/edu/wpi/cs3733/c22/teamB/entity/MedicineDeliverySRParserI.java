a1b91bd7a40be8d5005305e7e82f64d83861023d
hushmandesmaeili <hushmand6@gmail.com>
2022-02-13 01:31:54 -0500

    Implemented MedicineDeliverySRParserI

--- src/main/java/edu/wpi/cs3733/c22/teamB/entity/MedicineDeliverySRParserI.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/entity/MedicineDeliverySRParserI.java
@@ -1,27 +1,52 @@
 package edu.wpi.cs3733.c22.teamB.entity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MedicineDeliverySRParserI implements IParser<MedicineDeliverySR> {
 
-
     @Override
     public MedicineDeliverySR fromStringToObject(String string) {
-        return null;
+        MedicineDeliverySR medicineDeliverySR = new MedicineDeliverySR();
+
+        String[] data = string.split(",");
+
+        medicineDeliverySR.setSrID(data[0]);
+        medicineDeliverySR.setMedicineID(data[1]);
+        medicineDeliverySR.setPatientID(data[2]);
+
+        return medicineDeliverySR;
     }
 
     @Override
     public List<MedicineDeliverySR> fromStringsToObjects(List<String> listString) {
-        return null;
+        List<MedicineDeliverySR> medicineDeliverySRList =
+                listString.stream()
+                        .map(
+                                data_str -> {
+                                    return fromStringToObject(data_str);
+                                })
+                        .collect(Collectors.toList());
+
+        return medicineDeliverySRList;
     }
 
     @Override
-    public String fromObjectToString(MedicineDeliverySR object) {
-        return null;
+    public String fromObjectToString(MedicineDeliverySR medicineDeliverySR) {
+        String str = medicineDeliverySR.toStringFields();
+
+        return str;
     }
 
     @Override
     public List<String> fromObjectsToStrings(List<MedicineDeliverySR> listT) {
-        return null;
+        List<String> listString = new ArrayList<>();
+        listString.add(MedicineDeliverySR.toStringHeader());
+        for (MedicineDeliverySR medicineDeliverySR : listT) {
+            listString.add(medicineDeliverySR.toStringFields());
+        }
+
+        return listString;
     }
 }
