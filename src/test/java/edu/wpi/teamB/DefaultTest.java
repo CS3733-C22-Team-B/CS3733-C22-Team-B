46d6afa04d5d78740795adafeae1a503bdfb0c6f
Livingwell088 <58043224+Livingwell088@users.noreply.github.com>
2022-02-18 22:30:30 -0500

    Made simple password hashing work,testing, and made simple sign up page but not connected to anywhere yet.

--- src/test/java/edu/wpi/teamB/DefaultTest.java
+++ src/test/java/edu/wpi/teamB/DefaultTest.java
@@ -2,12 +2,24 @@
 /* DO NOT DELETE THIS TEST */
 /*-------------------------*/
 
-package edu.wpi.cs3733.c22.teamB;
+package edu.wpi.teamB;
 
+import edu.wpi.cs3733.c22.teamB.entity.DatabaseWrapper;
+import edu.wpi.cs3733.c22.teamB.entity.Employee;
+import edu.wpi.cs3733.c22.teamB.entity.PasswordHashing;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DefaultTest {
 
     @Test
-    public void test() {}
+    public void test() {
+        String password = "Aqwe12";
+        System.out.println(PasswordHashing.hashPassword(password));
+
+        DatabaseWrapper db = new DatabaseWrapper();
+        Employee employee = new Employee("ASD", "asd", "ad", "asd", 12, "asd", "asd", "Asd", "asd");
+//        db.addEmployee(employee);
+        assertEquals(db.getEmployee("ASD").getPassword(), PasswordHashing.hashPassword("asd"));
+    }
 }
