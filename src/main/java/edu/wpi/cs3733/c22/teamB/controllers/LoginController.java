b66e4bd69ddc91a50f31a95c93fd35ff5dfb3060
sitsanok <69603024+sitsanok@users.noreply.github.com>
2022-02-20 00:02:11 -0500

    putting this here in case we break something

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/LoginController.java
@@ -2,6 +2,9 @@ package edu.wpi.cs3733.c22.teamB.controllers;
 
 import edu.wpi.cs3733.c22.teamB.Bapp;
 import edu.wpi.cs3733.c22.teamB.Main;
+import edu.wpi.cs3733.c22.teamB.entity.DatabaseWrapper;
+import edu.wpi.cs3733.c22.teamB.entity.PasswordHashing;
+import edu.wpi.cs3733.c22.teamB.entity.objects.Employee;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,8 +15,11 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.List;
 
 public class LoginController {
+    String employeeUser;
+    String employeePass;
 
     @FXML
     private PasswordField passField;
@@ -27,23 +33,38 @@ public class LoginController {
     @FXML
     public void initialize(){
         Bapp.getPrimaryStage().setResizable(false);
+        DatabaseWrapper db = new DatabaseWrapper();
+        List<Employee> employeeList = db.getAllEmployee();
+        for(Employee employee : employeeList){
+            employeeUser = employee.getUsername();
+            employeePass = employee.getPassword();
+        }
+
     }
 
     @FXML
     void loginButton(ActionEvent event) {
-        if(passField.getText().isEmpty() || userField.getText().isEmpty()){
-            errorMessage.setText("Enter a username and password");
-        } else if((!passField.getText().equals("admin") || !userField.getText().equals("admin")) && (!passField.getText().equals("staff") || !userField.getText().equals("staff"))){
-            errorMessage.setText("Incorrect username or password");
-        } else{
-            String[] args = new String[2];
-            args[0] = userField.getText();
-            args[1] = passField.getText();
-            try {
-                Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/c22/teamB/views/borderHome.fxml"));
-                Bapp.getPrimaryStage().getScene().setRoot(root);
-            } catch (IOException ex) {
-                ex.printStackTrace();
+        DatabaseWrapper db = new DatabaseWrapper();
+        List<Employee> employeeList = db.getAllEmployee();
+        String pass = PasswordHashing.hashPassword(passField.getText());
+        for (Employee employee : employeeList) {
+            employeeUser = employee.getUsername();
+            employeePass = employee.getPassword();
+
+            if (passField.getText().isEmpty() || userField.getText().isEmpty()) {
+                errorMessage.setText("Enter a username and password"); //(!passField.getText().equals("admin") || !userField.getText().equals("admin")) && (!passField.getText().equals("staff") || !userField.getText().equals("staff")) &&
+            } else if ((!pass.equals(employeePass) || !userField.getText().equals(employeeUser))) {
+                errorMessage.setText("Incorrect username or password");
+            } else {
+                String[] args = new String[2];
+                args[0] = userField.getText();
+                args[1] = passField.getText();
+                try {
+                    Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/c22/teamB/views/borderHome.fxml"));
+                    Bapp.getPrimaryStage().getScene().setRoot(root);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
--- src/main/java/edu/wpi/cs3733/c22/teamB/entity/CSVReader.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/entity/CSVReader.java
@@ -1,4 +1,4 @@
-package edu.wpi.cs3733.c22.teamB.entity.csv;
+package edu.wpi.cs3733.c22.teamB.entity;
 
 import java.io.*;
 import java.util.ArrayList;
