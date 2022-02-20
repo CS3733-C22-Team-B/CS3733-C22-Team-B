47a0465e4067bb8f025a3efdae81127be3f5f4ca
Andrew <atnguyen@wpi.edu>
2022-02-20 13:46:29 -0500

    trying zoom

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/MapEditorController.java
@@ -9,6 +9,8 @@ import edu.wpi.cs3733.c22.teamB.entity.*;
 import edu.wpi.cs3733.c22.teamB.entity.inheritance.AbstractSR;
 import edu.wpi.cs3733.c22.teamB.entity.objects.Location;
 import edu.wpi.cs3733.c22.teamB.entity.objects.MedicalEquipment;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
@@ -16,8 +18,10 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -166,6 +170,95 @@ public class MapEditorController{
         modifyPopup.setStyle("-fx-padding: 1;");
         //1000
         //877
+        double width = imageView.getImage().getWidth();
+        double height = imageView.getImage().getHeight();
+        imageView.setPreserveRatio(true);
+        ObjectProperty<Point2D> mouseDown = new SimpleObjectProperty<>();
+
+        imageView.setOnMousePressed(e -> {
+
+            Point2D mousePress = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+            mouseDown.set(mousePress);
+        });
+
+        imageView.setOnMouseDragged(e -> {
+            Point2D dragPoint = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+            shift(imageView, dragPoint.subtract(mouseDown.get()));
+            mouseDown.set(imageViewToImage(imageView, new Point2D(e.getX(), e.getY())));
+        });
+
+        imageView.setOnScroll(e -> {
+            double delta = e.getDeltaY();
+            Rectangle2D viewport = imageView.getViewport();
+
+            double scale = clamp(Math.pow(1.01, delta),
+
+                    // don't scale so we're zoomed in to fewer than MIN_PIXELS in any direction:
+                    Math.min(10 / viewport.getWidth(), 10 / viewport.getHeight()),
+
+                    // don't scale so that we're bigger than image dimensions:
+                    Math.max(width / viewport.getWidth(), height / viewport.getHeight())
+
+            );
+
+            Point2D mouse = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+
+            double newWidth = viewport.getWidth() * scale;
+            double newHeight = viewport.getHeight() * scale;
+
+            // To keep the visual point under the mouse from moving, we need
+            // (x - newViewportMinX) / (x - currentViewportMinX) = scale
+            // where x is the mouse X coordinate in the image
+
+            // solving this for newViewportMinX gives
+
+            // newViewportMinX = x - (x - currentViewportMinX) * scale
+
+            // we then clamp this value so the image never scrolls out
+            // of the imageview:
+
+            double newMinX = clamp(mouse.getX() - (mouse.getX() - viewport.getMinX()) * scale,
+                    0, width - newWidth);
+            double newMinY = clamp(mouse.getY() - (mouse.getY() - viewport.getMinY()) * scale,
+                    0, height - newHeight);
+
+            imageView.setViewport(new Rectangle2D(newMinX, newMinY, newWidth, newHeight));
+        });
+    }
+    private double clamp(double value, double min, double max) {
+
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    }
+    // shift the viewport of the imageView by the specified delta, clamping so
+    // the viewport does not move off the actual image:
+    private void shift(ImageView imageView, Point2D delta) {
+        Rectangle2D viewport = imageView.getViewport();
+
+        double width = imageView.getImage().getWidth() ;
+        double height = imageView.getImage().getHeight() ;
+
+        double maxX = width - viewport.getWidth();
+        double maxY = height - viewport.getHeight();
+
+        double minX = clamp(viewport.getMinX() - delta.getX(), 0, maxX);
+        double minY = clamp(viewport.getMinY() - delta.getY(), 0, maxY);
+
+        imageView.setViewport(new Rectangle2D(minX, minY, viewport.getWidth(), viewport.getHeight()));
+    }
+
+    // convert mouse coordinates in the imageView to coordinates in the actual image:
+    private Point2D imageViewToImage(ImageView imageView, Point2D imageViewCoordinates) {
+        double xProportion = imageViewCoordinates.getX() / imageView.getBoundsInLocal().getWidth();
+        double yProportion = imageViewCoordinates.getY() / imageView.getBoundsInLocal().getHeight();
+
+        Rectangle2D viewport = imageView.getViewport();
+        return new Point2D(
+                viewport.getMinX() + xProportion * viewport.getWidth(),
+                viewport.getMinY() + yProportion * viewport.getHeight());
     }
 
     public void setTextPos(){
@@ -472,6 +565,8 @@ public class MapEditorController{
         updatePopup();
 
     }
+
+
 
 
 
