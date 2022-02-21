55ad491affff8901f8785b334f839720a90c6f5b
sitsanok <69603024+sitsanok@users.noreply.github.com>
2022-02-20 21:32:05 -0500

    added back button woohoo!

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/SignUpController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/SignUpController.java
@@ -1,19 +1,26 @@
 package edu.wpi.cs3733.c22.teamB.controllers;
 
 //import edu.wpi.cs3733.c22.teamB.entity.objects.AbstractSR;
+import edu.wpi.cs3733.c22.teamB.Bapp;
 import edu.wpi.cs3733.c22.teamB.entity.DatabaseWrapper;
+import edu.wpi.cs3733.c22.teamB.entity.SRIDGenerator;
 import edu.wpi.cs3733.c22.teamB.entity.objects.Employee;
 import edu.wpi.cs3733.c22.teamB.entity.inheritance.AbstractSR;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SignUpController implements IController, Initializable {
+public class SignUpController implements Initializable {
+
 
     @FXML private TextField employeeID;
     @FXML private TextField lastName;
@@ -25,28 +32,38 @@ public class SignUpController implements IController, Initializable {
     @FXML private TextField email;
     @FXML private TextField phoneNumber;
     @FXML private Button submitButton;
+    @FXML private Button backButton;
 
     public SignUpController(){};
 
-    @Override
-    public void submit() {
+    @FXML private void submit(ActionEvent actionEvent) {
         DatabaseWrapper db = new DatabaseWrapper();
         db.addEmployee(new Employee(employeeID.getText(), lastName.getText(), firstName.getText(), position.getText(), Integer.parseInt(accessLevel.getText()), username.getText(), password.getText(), email.getText(), phoneNumber.getText()));
-
+        clear();
     }
 
-    @Override
-    public void submit(AbstractSR sr) {
-
+    @FXML private void back(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/c22/teamB/views/Login.fxml"));
+            Bapp.getPrimaryStage().getScene().setRoot(root);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    @Override
     public void clear() {
-
+        employeeID.clear();
+        lastName.clear();
+        firstName.clear();
+        position.clear();
+        accessLevel.clear();
+        username.clear();
+        password.clear();
+        email.clear();
+        phoneNumber.clear();
     }
-
-    @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+//        employeeID.setText(.generateID());
     }
+
 }
