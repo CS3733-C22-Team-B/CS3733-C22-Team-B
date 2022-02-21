2080d0a9ec04f464f5fa52bd8b76dc6cd9946b5c
sitsanok <69603024+sitsanok@users.noreply.github.com>
2022-02-20 22:23:44 -0500

    pretty confirmation popup wooh :D

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/SignUpController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/SignUpController.java
@@ -6,6 +6,7 @@ import edu.wpi.cs3733.c22.teamB.entity.DatabaseWrapper;
 import edu.wpi.cs3733.c22.teamB.entity.SRIDGenerator;
 import edu.wpi.cs3733.c22.teamB.entity.objects.Employee;
 import edu.wpi.cs3733.c22.teamB.entity.inheritance.AbstractSR;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +14,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.io.IOException;
@@ -22,24 +25,34 @@ import java.util.ResourceBundle;
 public class SignUpController implements Initializable {
 
 
-    @FXML private TextField employeeID;
+
     @FXML private TextField lastName;
     @FXML private TextField firstName;
     @FXML private TextField position;
-    @FXML private TextField accessLevel;
     @FXML private TextField username;
     @FXML private TextField password;
     @FXML private TextField email;
     @FXML private TextField phoneNumber;
     @FXML private Button submitButton;
     @FXML private Button backButton;
+    @FXML private Pane popup;
 
     public SignUpController(){};
 
     @FXML private void submit(ActionEvent actionEvent) {
         DatabaseWrapper db = new DatabaseWrapper();
-        db.addEmployee(new Employee(employeeID.getText(), lastName.getText(), firstName.getText(), position.getText(), Integer.parseInt(accessLevel.getText()), username.getText(), password.getText(), email.getText(), phoneNumber.getText()));
+        db.addEmployee(new Employee(lastName.getText(), firstName.getText(), position.getText(), 1, username.getText(), password.getText(), email.getText(), phoneNumber.getText()));
+
+        // account confirmation popup
         clear();
+        setVisible(false);
+        PauseTransition visiblePause = new PauseTransition(
+                Duration.seconds(3)
+        );
+        visiblePause.setOnFinished(
+                event -> setVisible(true)
+        );
+        visiblePause.play();
     }
 
     @FXML private void back(ActionEvent actionEvent) {
@@ -52,16 +65,27 @@ public class SignUpController implements Initializable {
     }
 
     public void clear() {
-        employeeID.clear();
         lastName.clear();
         firstName.clear();
         position.clear();
-        accessLevel.clear();
         username.clear();
         password.clear();
         email.clear();
         phoneNumber.clear();
     }
+
+    public void setVisible(boolean bool) {
+        lastName.setVisible(bool);
+        firstName.setVisible(bool);
+        position.setVisible(bool);
+        username.setVisible(bool);
+        password.setVisible(bool);
+        email.setVisible(bool);
+        phoneNumber.setVisible(bool);
+        popup.setVisible(!bool);
+    }
+
     public void initialize(URL location, ResourceBundle resources) {
+        setVisible(true);
     }
 }
