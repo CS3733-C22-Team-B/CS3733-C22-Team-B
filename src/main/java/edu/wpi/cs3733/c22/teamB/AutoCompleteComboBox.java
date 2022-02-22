16e71d6929bf05254ac3abfba802bfcf3facf099
dtbpkmte <dtbpkmte@gmail.com>
2022-02-22 15:21:48 -0500

    generalized AutoComplete class

--- src/main/java/edu/wpi/cs3733/c22/teamB/AutoCompleteComboBox.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/AutoCompleteComboBox.java
@@ -6,12 +6,14 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class AutoCompleteComboBox<T> {
     private JFXComboBox<T> box;
     private JFXComboBoxListViewSkin<T> boxSkin;
     private List<T> data;
+    private Function<T, String> func = (t -> t.toString());
     private int minCharsToTrigger = 3;
     private T prevSelection;
 
@@ -22,6 +24,12 @@ public class AutoCompleteComboBox<T> {
         this.data = data;
         init();
     }
+    public AutoCompleteComboBox(JFXComboBox<T> box, List<T> data, Function<T, String> f) {
+        this.box = box;
+        this.data = data;
+        this.func = f;
+        init();
+    }
     public AutoCompleteComboBox(JFXComboBox<T> box, List<T> data, int minCharsToTrigger) {
         this.box = box;
         this.data = data;
@@ -29,7 +37,7 @@ public class AutoCompleteComboBox<T> {
         init();
     }
 
-    public void init() {
+    private void init() {
         box.setEditable(true);
         box.getItems().addAll(data);
         boxSkin = new JFXComboBoxListViewSkin<T>(box);
@@ -56,7 +64,7 @@ public class AutoCompleteComboBox<T> {
                     box.getItems().removeAll();
                     box.getItems().addAll(data.stream().filter(t -> {
                         String s = box.getEditor().getText().toLowerCase();
-                        return t.toString().toLowerCase().contains(s);
+                        return func.apply(t).toLowerCase().contains(s);
                     }).collect(Collectors.toList()));
                     fullData = false;
                     boxSkin.show();
