0dce60930c0da277f5bc51c0a3599da049690c3a
dtbpkmte <dtbpkmte@gmail.com>
2022-02-22 22:56:45 -0500

    done?

--- src/main/java/edu/wpi/cs3733/c22/teamB/workflow/EquipmentMoveTask.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/workflow/EquipmentMoveTask.java
@@ -25,9 +25,14 @@ public class EquipmentMoveTask extends AbstractTask implements Runnable {
     @Override
     public void run() {
         try {
+            DatabaseWrapper dw = new DatabaseWrapper();
+
             // first complete the SR
             System.out.println("Service " + sr.getSrID() + " starting");
             Thread.sleep(ThreadLocalRandom.current().nextInt(5000, 10001));
+
+            sr.setStatus("IN PROGRESS");
+            dw.updateSR(sr);
 
             System.out.println(
                             "Service " +
@@ -38,13 +43,18 @@ public class EquipmentMoveTask extends AbstractTask implements Runnable {
                             sr.getLocation().getLongName());
 
             sr.getMedicalEquipment().setLocation(sr.getLocation());
-            sr.getMedicalEquipment().setStatus("DIRTY");
             sr.setStatus("DONE");
+
+            dw.updateMedicalEquipment(sr.getMedicalEquipment());
+            dw.updateSR(sr);
 
             // patient using equipment
             System.out.println("Patient is using equipment...");
             Thread.sleep(ThreadLocalRandom.current().nextInt(5000, 10001));
-            System.out.printf("Patient finished using equipment.");
+            System.out.println("Patient finished using equipment.");
+
+            sr.getMedicalEquipment().setStatus("DIRTY");
+            dw.updateMedicalEquipment(sr.getMedicalEquipment());
 
             // then move by specified path
             System.out.println("Equipment " +
@@ -58,19 +68,16 @@ public class EquipmentMoveTask extends AbstractTask implements Runnable {
                                 sr.getMedicalEquipment().getLocation().getLongName() + " to " +
                                 loc.getLongName());
                 sr.getMedicalEquipment().setLocation(loc);
+                dw.updateMedicalEquipment(sr.getMedicalEquipment());
             }
         } catch (InterruptedException e) {
 
         }
-//        DatabaseWrapper dw = new DatabaseWrapper();
-//        dw.updateMedicalEquipment(sr.getMedicalEquipment());
-//        dw.updateSR(sr);
         System.out.println("Task completed.");
     }
 
     @Override
     public void start() {
-
         if (t == null) {
             t = new Thread(this);
             t.start();
--- src/main/java/edu/wpi/cs3733/c22/teamB/workflow/Workflow.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/workflow/Workflow.java
@@ -2,23 +2,18 @@ package edu.wpi.cs3733.c22.teamB.workflow;
 
 import edu.wpi.cs3733.c22.teamB.entity.objects.Location;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.*;
 
 public class Workflow {
     public static final List<String> infusionPumpPath = List.of("HBATH00203", "bSTOR00101", "bSTOR00103");
-    private ExecutorService exec = Executors.newSingleThreadExecutor();
 
     public void addTask(AbstractTask task) {
-        exec.submit(task);
+        task.start();
     }
 
     public void addAllTasks(Collection<AbstractTask> taskCollection) {
         for (AbstractTask task : taskCollection) {
-            exec.submit(task);
+            task.start();
         }
     }
 }
