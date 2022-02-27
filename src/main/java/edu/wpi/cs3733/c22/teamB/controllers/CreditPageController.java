3e39b64a3dc9e4c226c286de143349b1230bf3d7
sitsanok <69603024+sitsanok@users.noreply.github.com>
2022-02-27 00:57:22 -0500

    added credits page template since we haven't added the apis yet

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/CreditPageController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/CreditPageController.java
@@ -1,4 +1,17 @@
 package edu.wpi.cs3733.c22.teamB.controllers;
 
-public class CreditPageController {
+import javafx.fxml.FXML;
+
+public class CreditPageController extends AbsPage{
+    @Override
+    public void namePage() {
+        AnchorHomeController.curAnchorHomeController.pageName.setText("Credits Page");
+    }
+
+    @FXML
+    private void initialize() {
+        initResize();
+        resize();
+        namePage();
+    }
 }
