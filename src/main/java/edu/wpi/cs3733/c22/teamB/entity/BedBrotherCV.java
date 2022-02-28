7904336710432e986f147ab93fccfc7b4ae05123
Calvin Page <97639112+calvinpage@users.noreply.github.com>
2022-02-27 19:31:17 -0500

    broken still

--- src/main/java/edu/wpi/cs3733/c22/teamB/entity/BedBrotherCV.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/entity/BedBrotherCV.java
@@ -3,10 +3,11 @@ package edu.wpi.cs3733.c22.teamB.entity;
 import java.util.Scanner;
 import com.fazecast.jSerialComm.SerialPort;
 
-public class BedBrotherCV {
+public class BedBrotherCV extends Thread{
 
     static SerialPort chosenPort;
     public void call(){
+        System.out.println("e");
         chosenPort = SerialPort.getCommPort("COM5");
         chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER,0,0);
         if(chosenPort.openPort()){
