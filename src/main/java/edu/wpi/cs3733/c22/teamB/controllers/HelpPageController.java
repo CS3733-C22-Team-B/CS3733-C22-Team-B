c988a79fdfd9c4d67f3ccf535862f758478127f4
Noah Leibowitz <nleibowitz@wpi.edu>
2022-02-28 14:41:00 -0500

    changed credits a bit, created empty list for image paths

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/HelpPageController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/HelpPageController.java
@@ -1,5 +1,17 @@
 package edu.wpi.cs3733.c22.teamB.controllers;
 
-public class HelpPageController {
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.util.List;
+
+public class HelpPageController {
+@FXML
+    ImageView image;
+List<Image> imagePaths;
+
+    public void showPage (int index){
+        image.setImage(imagePaths.get(index));
+    }
 }
