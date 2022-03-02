d39ddc545fec72168525641bed93ea98b8ea73e5
Calvin Page <97639112+calvinpage@users.noreply.github.com>
2022-03-01 22:34:09 -0500

    hotfixthreAD

--- src/main/java/edu/wpi/cs3733/c22/teamB/controllers/AnchorHomeController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamB/controllers/AnchorHomeController.java
@@ -42,8 +42,8 @@ public class AnchorHomeController implements Initializable {
     @FXML public VBox sidebar;
     @FXML JFXButton gameButton;
     FXMLLoader mapLoader = new FXMLLoader(getClass().getResource(pageToFXMLPath("MapEditor")));
-    BedBrotherCV equipVision = new BedBrotherCV();
-    Thread visionThread = new Thread(equipVision);
+    static BedBrotherCV equipVision = new BedBrotherCV();
+    public static Thread visionThread = new Thread(equipVision);
 
     Popup popup = new Popup();
     Pane popUpMessage = new FXMLLoader(getClass().getResource(pageToFXMLPath("LogOutPopUp"))).load();
@@ -281,6 +281,7 @@ public class AnchorHomeController implements Initializable {
     }
 
     public void exit(ActionEvent actionEvent) {
+        visionThread.stop();
         Platform.exit();
     }
 
